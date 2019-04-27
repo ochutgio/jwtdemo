09fad5cc31d297a14632d6a2094881cb351b6615
ochutgio <ochutgio@gmail.com>
2019-04-27 15:57:01 +0700

    . add jwt configuration

--- JsonWebTokenDemo/Controllers/AuthController.cs
+++ JsonWebTokenDemo/Controllers/AuthController.cs
@@ -15,6 +15,12 @@ namespace JsonWebTokenDemo.Controllers
     [ApiController]
     public class AuthController : Controller
     {
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public IActionResult Index()
         {
             return View();
@@ -25,25 +31,34 @@ namespace JsonWebTokenDemo.Controllers
         {
 
             // security key
-            string securityKey = "this_is_very_long_security_key_for_token_validation_project";
+            // string securityKey = "this_is_very_long_security_key_for_token_validation_project";
             // symmetric security key
-            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JsonWebToken:SigningKey"]));
             // var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JsonWebToken:SigningKey"]));
             // signing credentials
             var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+
             // claim
             var claims = new List<Claim>();
             claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
             claims.Add(new Claim("id", "110"));
             claims.Add(new Claim("our_custom_claim", "our_custom_values"));
             // create token
+            //var token = new JwtSecurityToken(
+            //    issuer: "localhost",
+            //    audience: "readers",
+            //    expires: DateTime.Now.AddHours(1),
+            //    claims: claims,
+            //    signingCredentials: signingCredentials
+            //    );
             var token = new JwtSecurityToken(
-                issuer: "localhost",
-                audience: "readers",
-                expires: DateTime.Now.AddHours(1),
-                claims: claims,
-                signingCredentials: signingCredentials
-                );
+               issuer: Configuration["JsonWebToken:Issuer"],
+               audience: Configuration["JsonWebToken:Audience"],
+               expires: DateTime.Now.AddHours(1),
+               claims: claims,
+               signingCredentials: signingCredentials
+               );
             // return token
 
             return Ok(new JwtSecurityTokenHandler().WriteToken(token));

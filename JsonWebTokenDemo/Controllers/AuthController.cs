689d275202a2c0db3893dfcc2b4813707c256acc
ochutgio <ochutgio@gmail.com>
2019-04-27 15:29:53 +0700

    JsonWebToken

---
+++ JsonWebTokenDemo/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JsonWebTokenDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("token")]
+        public IActionResult GetToken()
+        {
+
+            // security key
+            string securityKey = "this_is_very_long_security_key_for_token_validation_project";
+            // symmetric security key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            // var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JsonWebToken:SigningKey"]));
+            // signing credentials
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            // claim
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            claims.Add(new Claim("id", "110"));
+            claims.Add(new Claim("our_custom_claim", "our_custom_values"));
+            // create token
+            var token = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "readers",
+                expires: DateTime.Now.AddHours(1),
+                claims: claims,
+                signingCredentials: signingCredentials
+                );
+            // return token
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
---
+++ jwtdemo/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jwtdemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Token")]
+        public IActionResult GetToken()
+        {
+
+            // security key
+
+            // symmetric security key
+
+            // signing credentials
+
+            return Ok("Hello from API") ;
+        }
+    }
+}
